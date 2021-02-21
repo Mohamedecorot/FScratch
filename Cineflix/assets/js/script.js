1733417e88fda0eb869c9226e4ef556973cc9014
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-02-21 17:30:02 +0100

    projet cineflix: amélioration de la section 'foire aux questions' (js)

--- Cineflix/assets/js/script.js
+++ Cineflix/assets/js/script.js
@@ -1,11 +1,18 @@
 console.log("coucou")
-
+console.log(document.querySelector(".answer"))
 function afficher_cacher(id)
 {
     if(document.getElementById(id).style.display=="none") {
+        document.getElementById('answer1').style.display="none";
+        document.getElementById('answer2').style.display="none";
+        document.getElementById('answer3').style.display="none";
+        document.getElementById('answer4').style.display="none";
+        document.getElementById('answer5').style.display="none";
         document.getElementById(id).style.display="block";
+        document.getElementById('bouton_'+id).innerHTML='-';
     } else {
         document.getElementById(id).style.display="none";
+        document.getElementById('bouton_'+id).innerHTML='+';
     }
     return true;
 }
