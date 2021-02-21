1a3d6f56942276d558adc09ed3902cb33eafcad1
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-02-21 21:11:16 +0100

    projet cineflix: ajout du footer (1ere partie)' (html et css)

--- Cineflix/assets/js/script.js
+++ Cineflix/assets/js/script.js
@@ -9,10 +9,9 @@ function afficher_cacher(id)
         document.getElementById('answer4').style.display="none";
         document.getElementById('answer5').style.display="none";
         document.getElementById(id).style.display="block";
-        document.getElementById('bouton_'+id).innerHTML='-';
     } else {
         document.getElementById(id).style.display="none";
-        document.getElementById('bouton_'+id).innerHTML='+';
     }
     return true;
 }
+// http://www.supportduweb.com/scripts_tutoriaux-code-source-33-afficher-cacher-un-div-element-en-javascript.html
