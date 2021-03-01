4e17ae2c6c613ce2e1b4aeb422e27f62a9a8bb4e
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-03-01 17:49:58 +0100

    CV_React : ajout du rooter avec les différentes pages

---
+++ 04cv_react/src/pages/Knowledges.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Knowledges = () => {
+    return (
+        <div>
+            Hello depuis connaissance
+        </div>
+    );
+};
+
+export default Knowledges;
