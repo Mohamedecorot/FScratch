9bec547abd1049c7f9fefb29f6af075fa9ed4c86
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-03-04 21:24:16 +0100

    ajout du projet bouton-animé-js-css

---
+++ 00Divers/JS/JS-bouton-animé/script.js
@@ -0,0 +1,12 @@
+document.getElementById('button').onmousemove = (e) => {
+
+	const x = e.pageX - e.target.offsetLeft
+	const y = e.pageY - e.target.offsetTop
+	
+	console.log(x + '/' + y);
+
+  e.target.style.setProperty('--x', `${ x }px`)
+	e.target.style.setProperty('--y', `${ y }px`)
+	
+	console.log(e);
+}
