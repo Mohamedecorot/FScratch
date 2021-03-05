c4ff44f1ec0bf5d1db2988ea37faab2de667cf8c
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-03-05 17:42:37 +0100

    projet cia : ajout du header (js)

---
+++ cia/script.js
@@ -0,0 +1,15 @@
+// Disparation de la navbar au scroll
+
+let header = document.querySelector(".header");
+let lastScrollValue = 0;
+
+document.addEventListener('scroll',() => {
+    let top  = document.documentElement.scrollTop;
+    if(lastScrollValue < top) {
+    	header.classList.add("hidden");
+    } else {
+    	header.classList.remove("hidden");
+    }
+    lastScrollValue = top;
+});
+
