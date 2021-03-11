e2eea4bd8cab88a7c98dcdd8a8beb05786c88079
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-03-11 14:42:54 +0100

    projet cia : changement du logo de la navbar au scroll

--- cia/script.js
+++ cia/script.js
@@ -3,14 +3,29 @@
 let header = document.querySelector(".header");
 let lastScrollValue = 0;
 
+let logocia = document.querySelector(".logo-cia");
+let cia = document.querySelector(".cia");
+let ciahidden = document.querySelector(".cia-hidden");
+
 document.addEventListener('scroll',() => {
     let top  = document.documentElement.scrollTop;
+    //disparition et apparition de la navbar au scroll
     if(lastScrollValue < top) {
     	header.classList.add("hidden");
     } else {
     	header.classList.remove("hidden");
     }
     lastScrollValue = top;
+    //modification du logo de la navbar selon le scroll
+    if(window.scrollY > 40) {
+        logocia.style.display="none";
+        cia.style.display="none";
+        ciahidden.style.display="block";
+    } else {
+        logocia.style.display="block";
+        cia.style.display="block";
+        ciahidden.style.display="none";
+    }
 });
 
 // Affichage aléatoire de l'image du heros
