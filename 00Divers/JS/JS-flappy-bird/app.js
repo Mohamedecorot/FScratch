38b79b57eaee0c58025331b466b8607eb87d2d22
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-03-14 07:53:03 +0100

    projet flappy-bird : ajout de l'animation du background

--- 00Divers/JS/JS-flappy-bird/app.js
+++ 00Divers/JS/JS-flappy-bird/app.js
@@ -21,7 +21,13 @@ let flyHeight;
 const render = () => {
     index ++;
 
+    // animation du background
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed / 2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed / 2)) % canvas.width), 0, canvas.width, canvas.height);
 
+
+
+    // animation de l'oiseau qui vole
     ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width / 2) - size [0] / 2), flyHeight, ...size);
     flyHeight = (canvas.height / 2) - (size[1] / 2);
     window.requestAnimationFrame(render);
