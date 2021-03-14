b7fb4455ed64c103ab2fd8ed013db270f15618ef
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-03-14 08:05:23 +0100

    projet flappy-bird : ajout de l'animation de l'oiseau qui se deplace verticalement au clique

--- 00Divers/JS/JS-flappy-bird/app.js
+++ 00Divers/JS/JS-flappy-bird/app.js
@@ -25,11 +25,27 @@ const render = () => {
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed / 2)) % canvas.width) + canvas.width, 0, canvas.width, canvas.height);
     ctx.drawImage(img, 0, 0, canvas.width, canvas.height, -((index * (speed / 2)) % canvas.width), 0, canvas.width, canvas.height);
 
+    if (gamePlaying) {
+        // déplacement de l'oiseau vers la gauche au lancement du jeu
+        ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, cTenth, flyHeight, ...size);
+        flight += gravity;
+        // ajout de la gravité qui tire l'oiseau vers le bas
+        flyHeight = Math.min(flyHeight + flight, canvas.height - size[1]);
 
+    } else {
+        // animation de l'oiseau qui vole
+        ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width / 2) - size [0] / 2), flyHeight, ...size);
+        flyHeight = (canvas.height / 2) - (size[1] / 2);
 
-    // animation de l'oiseau qui vole
-    ctx.drawImage(img, 432, Math.floor((index % 9) / 3) * size[1], ...size, ((canvas.width / 2) - size [0] / 2), flyHeight, ...size);
-    flyHeight = (canvas.height / 2) - (size[1] / 2);
+        // Ajout de texte sur le canvas
+        ctx.fillText(`Meilleur score : ${bestScore}`, 55, 245);
+        ctx.fillText('Cliquez pour jouer', 48, 535);
+        ctx.font = "bold 30px courier";
+    }
+
     window.requestAnimationFrame(render);
 }
+
 img.onload = render;
+document.addEventListener('click', () => gamePlaying = true)
+window.onclick = () => flight = jump;
