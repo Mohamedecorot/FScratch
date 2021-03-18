aff0f5f524ea3646f2d3fdec43ef784c948352da
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-03-18 09:04:34 +0100

    Input radio

--- 06React-Api-project/src/components/Countries.js
+++ 06React-Api-project/src/components/Countries.js
@@ -7,6 +7,9 @@ const Countries = () => {
     const [sortedData, setSortedData] = useState([]);
     const [playOnce, setPlayOne] = useState(true);
     const [rangeValue, setRangeValue] = useState(40);
+    const [selectedRadio, setSelectedRadio] = useState('');
+    const radios = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+
 
     useEffect(() => {
         if (playOnce) {
@@ -30,11 +33,32 @@ const Countries = () => {
 
     return (
         <div className="countries">
-            <div className="sort-contain">
+            <div className="sort-container">
                 <input type="range" min="1" max="250" value={rangeValue} onChange={(e) => setRangeValue(e.target.value)}/>
+                <ul>
+                    {radios.map((radio) => {
+                        return (
+                            <li key={radio}>
+                                <input
+                                    type="radio"
+                                    value={radio}
+                                    id={radio}
+                                    checked={radio === selectedRadio}
+                                    onChange={(e) => setSelectedRadio(e.target.value)}
+                                />
+                                <label htmlFor={radio}>{radio}</label>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            <div className="cancel">
+                {selectedRadio && <h5 onClick={() =>setSelectedRadio("")}>Annuler la recherche</h5>}
             </div>
             <ul className="countries-list">
-                {sortedData.map((country) => (
+                {sortedData
+                .filter((country) => country.region.includes(selectedRadio))
+                .map((country) => (
                     <Cards country={country} key={country.name}/>
                 ))}
             </ul>
